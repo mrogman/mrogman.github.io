b2e393a1b58e74540a4e372f1698764a2f550fa7
d30xy <mrogman@gmail.com>
2015-07-31 21:41:01 -0700

    install foundation plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,6 +67,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate4:4.3.10" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
+        runtime ":foundation:5.2.2"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         provided ':sass-asset-pipeline:2.3.1'
